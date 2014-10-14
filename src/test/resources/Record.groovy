2bae121e94e86bd42db72a1a12aca8a05471c14c
Anton Hrytsenko <hrytsenko.anton@gmail.com>
2014-10-14 15:21:34 +0300

    Add more cases into test for record.

--- src/test/resources/Record.groovy
+++ src/test/resources/Record.groovy
@@ -1,3 +1,12 @@
-def record = record( ticker : "ORCL", name : "Oracle" )
+def record = record(ticker : "ORCL", name : "Oracle")
 
 assert record.ticker == record["ticker"]
+
+record.exchange = "NYSE"
+assert record.exchange == "NYSE"
+
+record.exchange = null
+assert record.exchange == "null"
+
+record.remove("exchange")
+assert record.exchange == null
