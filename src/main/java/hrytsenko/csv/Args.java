af9b4a34dcb5641b8f6b71720f1a0b4341acd097
Anton Hrytsenko <hrytsenko.anton@gmail.com>
2014-12-10 22:05:38 +0200

    Fix minor issues with FindBugs and License plugins.

--- src/main/java/hrytsenko/csv/Args.java
+++ src/main/java/hrytsenko/csv/Args.java
@@ -1,5 +1,25 @@
+/*
+ * #%L
+ * csv-scripts
+ * %%
+ * Copyright (C) 2014 Anton Hrytsenko
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package hrytsenko.csv;
 
+import static java.util.Arrays.copyOf;
 import static org.apache.commons.cli.Option.UNLIMITED_VALUES;
 
 import org.apache.commons.cli.BasicParser;
@@ -37,8 +57,8 @@ public class Args {
      *            the list of string values.
      */
     public Args(String[] scripts, String[] values) {
-        this.scripts = scripts;
-        this.values = values;
+        this.scripts = copyOf(scripts, scripts.length);
+        this.values = copyOf(values, values.length);
     }
 
     /**
@@ -47,7 +67,7 @@ public class Args {
      * @return the list of filenames.
      */
     public String[] getScripts() {
-        return scripts;
+        return copyOf(scripts, scripts.length);
     }
 
     /**
@@ -56,7 +76,7 @@ public class Args {
      * @return the list of string values.
      */
     public String[] getValues() {
-        return values;
+        return copyOf(values, values.length);
     }
 
     /**
