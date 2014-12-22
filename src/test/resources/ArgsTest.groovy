a1b5d8cfcdc5be54dc7fbf6c2e8bac0d32f31efb
Anton Hrytsenko <hrytsenko.anton@gmail.com>
2014-12-22 21:37:39 +0200

    Use `log` in test.

--- src/test/resources/ArgsTest.groovy
+++ src/test/resources/ArgsTest.groovy
@@ -1,3 +1,8 @@
 def records = load(path: args[0]).group('exchange')
 
-assert records[args[1]].size() == args[2].toInteger()
+def exchange = args[1]
+def found = records[exchange].size()
+
+log "Found $found stocks for $exchange."
+
+assert args[2].toInteger() == found
